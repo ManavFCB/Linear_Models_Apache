641543b825eec9e10c2aa3be00d3b014c004a27f
ManavFCB <manavsrini@gmail.com>
2020-05-02 23:38:12 -0400

    Update LogReg.scala

--- LogReg.scala
+++ LogReg.scala
@@ -7,7 +7,7 @@ object LogReg {
   def main(args: Array[String]) {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
     if (args.length != 2) {
-      logger.info("Usage:\nwc.WordCountMain <input dir> <output dir>")
+      logger.info("Usage:\nLogistic Reg <input dir> <output dir>")
       System.exit(1)
     }
     val conf = new SparkConf()
