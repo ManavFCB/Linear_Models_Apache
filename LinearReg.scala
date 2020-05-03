9c5c571a4000ab0482937d7d218a6137806e8eb1
ManavFCB <manavsrini@gmail.com>
2020-05-02 23:37:17 -0400

    Update LinearReg.scala

--- LinearReg.scala
+++ LinearReg.scala
@@ -8,7 +8,7 @@ object LinearReg {
   def main(args: Array[String]) {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
     if (args.length != 2) {
-      logger.error("Usage:\nwc.WordCountMain <input dir> <output dir>")
+      logger.error("Usage:\nLinearReg main <input dir> <output dir>")
       System.exit(1)
     }
     val conf = new SparkConf()
